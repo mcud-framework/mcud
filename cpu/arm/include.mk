be3084ed24e7fb95ef9c9352d8e72b7dcd9a460e
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-10-08 11:27:33 +0200

    Add missing copyright notices

--- cpu/arm/include.mk
+++ cpu/arm/include.mk
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 TARGET = arm-none-eabi-
 DOCKER = seeseemelk/mcud:arm-none-eabi-2021-07-31
 DIRS += $(CPUS)/arm
